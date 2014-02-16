16b679ffccd1a704ba7850ad3db98790aee13281
Donald Cameron <cameron.donald77@gmail.com>
2014-02-15 23:40:44 -0700

    Fleshed out README; cleanup of Evaluate Reverse Polish Notation

--- EvaluateReversePolishNotation/eval.h
+++ EvaluateReversePolishNotation/eval.h
@@ -9,7 +9,7 @@ private:
 
 	long str_to_int(string & nstr, unsigned spos = 0) {
 		long num = 0;
-		for (int j = spos; j < nstr.size(); ++j)
+		for (unsigned j = spos; j < nstr.size(); ++j)
 			num = num * 10 + nstr[j] - '0';
 		return num;
 	}
@@ -18,7 +18,6 @@ public:
 
     int evalRPN(vector<string> &tokens)
 	{
-		long answer;
 		vector<long> numstack; // assume this is available
 
 		for (unsigned i = 0; i < tokens.size(); ++i)
@@ -26,8 +25,7 @@ public:
 		  	switch (tokens[i][0]) {
 				case '0': case '1': case '2': case '3': case '4':
 				case '5': case '6': case '7': case '8': case '9': {
-					long num = str_to_int(tokens[i]);
-					numstack.push_back(num);
+					numstack.push_back(str_to_int(tokens[i]));
 					break;
 				}
 				case '-': {
@@ -54,38 +52,22 @@ public:
 					numstack.pop_back();
 					switch (tokens[i][0]) {
 						case '*' : {
-							long expr = right * left;
-							numstack.push_back(expr);
+							numstack.push_back(right * left);
 							break;
 						}
 						case '/' : {
-							long expr = right / +left;
-							numstack.push_back(expr);
+							numstack.push_back(right / left);
 							break;
 						}
 						case '+' : {
-							long expr = right + left;
-							numstack.push_back(expr);
+							numstack.push_back(right + left);
 							break;
 						}
-						case '-' : {
-							long expr;
-							// check for unary minus
-							if (tokens[i].size() > 1) {
-								expr = -str_to_int(tokens[i], 1);
-							}
-							else {
-								expr = right - left;
-							}
-							numstack.push_back(expr);
-							break;
-						}
-						
 					} // inner switch
-				} // case +, -, *, /
+					break;
+				} // case +, *, /
 			} // outer switch
 		}
 		return numstack.back();
 	} // evalRPN
 };
-
--- README.txt
+++ README.txt
@@ -1,9 +1,14 @@
 README for Donald Cameron's leetcode repo
 
-If you found this repo by Googling, may I suggest that you try to work on
-leetcode problems yourself, and look at my (or anyone else's) solutions
-after you get stuck, or to compare after you have a complete solution.
+Certain people know about this repo.  If you simply found it by searching
+for leetcode, may I suggest that you try to work on leetcode problems yourself,
+and look at my (or anyone else's) solutions only after you get stuck, or to
+compare after you have a complete solution.
 
-You may email me through gitHub.  I cannot promise to respond, but I will
-try to do so as time permits.
+If you want to discuss general aspects of this or other leetcode problems, the best
+place to do that is on the leetcode online judge site.
+
+On the other hand, if there are aspects of my solutions you want to discuss
+or critique, you may email me through gitHub.  I cannot promise to respond,
+but I will try to do so as time permits.
 
